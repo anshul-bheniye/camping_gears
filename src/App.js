efb6fc7b3bad916ca89cfaf3f95c4af6a896ab31
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-17 15:20:35 +0530

    add dark mode on App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { AllRoutes } from './routes/AllRoutes';
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className="App dark:bg-slate-800">
       <Header />
       <AllRoutes/>
       <Footer />
