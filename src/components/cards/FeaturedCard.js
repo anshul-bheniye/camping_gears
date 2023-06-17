cf900b88d5cd05e573a5fdb44a12097b986a9f5d
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-17 15:18:13 +0530

    update file

--- src/components/cards/FeaturedCard.js
+++ src/components/cards/FeaturedCard.js
@@ -1,27 +1,23 @@
-import { Link } from "react-router-dom";
+
+import { Rating } from "../other";
 
 export const FeaturedCard = ({featuredGear}) => {
-  const {id, name, overview,image, price, rating, bestSeller} = featuredGear;
-
-  // rating start
-
-
-  // rating end
+  const {name, overview,image, price, ratings, bestSeller} = featuredGear;
 
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <Link to={`/gear/${id}`} className="relative" >
+        <div className="relative" >
             { bestSeller && <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">Best Seller</span> }
             <img className="rounded-t-lg w-full h-64" src={image} alt={name} />
-        </Link>
+        </div>
         <div className="p-5">
-            <Link to={`/gear/${id}`}>
+            <div>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-            </Link>
+            </div>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
             
             <div className="flex items-center my-2">
-                 {rating} 
+                 <Rating ratings={ratings}/>
             </div>
 
             <p className="flex justify-between items-center">
